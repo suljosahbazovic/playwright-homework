e2a0932e22de9d5c06b0b7265076cb5908471a29
Suljo Sahbazovic <suljo@zendev.se>
2026-09-10 09:07:11 +0200

    fixed and clean code

--- tests/datepicker.spec.ts
+++ tests/datepicker.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { time } from 'console';
 
 test.beforeEach( async({page}) => {
     await page.goto('/')
@@ -16,22 +17,21 @@ test.beforeEach( async({page}) => {
         await page.getByRole('link', { name: 'Harold Davis' }).click()
 
         //3. On the Owner Information page, select the "Add New Pet" button
+        await page.waitForResponse('**/api/owners/**')
         await page.getByRole('button', { name: 'Add New Pet' }).click()
-
+        
         //4. In the Name field, type any new pet name, for example, "Tom"
-        await page.getByRole('textbox', { name: 'Name' }).fill('Tom')
-
         //5. Add the assertion of icon in the input field, that it changed from "X" to "V"
-        await expect(page.locator('.glyphicon-ok')).toBeVisible()
+        await expect(page.locator('#name + span')).toHaveClass(/glyphicon-remove/)
+        await page.getByRole('textbox', { name: 'Name' }).fill('Tom')
+        await expect(page.locator('#name + span')).toHaveClass(/glyphicon-ok/)
 
         //6. Click on the calendar icon for the "Birth Date" field
         await page.getByRole('button', { name: 'Open calendar' }).click()
 
-        const calendarHeader = page.locator('mat-calendar-header')
-        const chooseDateYearButton = calendarHeader.locator('.mat-calendar-period-button')
-        const choosePreviousButton = calendarHeader.locator('.mat-calendar-previous-button')
-        await chooseDateYearButton.click()
-        await choosePreviousButton.click()
+        //7. Using the calendar selector, select the date "May 2nd, 2014"
+        await page.getByRole('button', {name: 'Choose month and year'}).click()
+        await page.getByRole('button', {name: 'Previous 24 years'}).click()
         await page.getByRole('button', { name: '2014' }).click()
         await page.getByRole('gridcell', { name: '05 2014' }).click()
         await page.getByRole('gridcell', { name: '2014/05/02' }).click()
@@ -45,9 +45,9 @@ test.beforeEach( async({page}) => {
 
         //10. On the Owner Information page, add assertions for the newly created pet. Name is Tom, Birth Date is in the format "2014-05-02", Type is dog
         const tomPetSection = page.locator('app-pet-list', { hasText: 'Tom' })
-        await expect(tomPetSection.locator('dt').filter({ hasText: 'Name' }).locator('+ dd')).toHaveText('Tom')
-        await expect(tomPetSection.locator('dt').filter({ hasText: 'Birth Date' }).locator('+ dd')).toHaveText('2014-05-02')
-        await expect(tomPetSection.locator('dt').filter({ hasText: 'Type' }).locator('+ dd')).toHaveText('dog')
+        await expect(tomPetSection.locator('dt', { hasText: 'Name' }).locator('+ dd')).toHaveText('Tom')
+        await expect(tomPetSection.locator('dt', { hasText: 'Birth Date' }).locator('+ dd')).toHaveText('2014-05-02')
+        await expect(tomPetSection.locator('dt', { hasText: 'Type' }).locator('+ dd')).toHaveText('dog')
 
         //11. Click the "Delete Pet" button for the new pet "Tom"
         await tomPetSection.getByRole('button', { name: 'Delete Pet' }).click()
@@ -68,48 +68,46 @@ test.beforeEach( async({page}) => {
         await expect(page.getByRole('heading', { name: 'New Visit' })).toBeVisible()
 
         //5. Add the assertion that the pet name is "Samantha" and owner's name is "Jean Coleman"
-        await expect(page.locator('app-visit-add').filter({ hasText: 'Pet' }).getByText('Samantha')).toBeVisible()
-        await expect(page.locator('app-visit-add').filter({ hasText: 'Pet' }).getByText('Jean Coleman')).toBeVisible()
+        await expect(page.locator('app-visit-add', { hasText: 'Pet' }).getByText('Samantha')).toBeVisible()
+        await expect(page.locator('app-visit-add', { hasText: 'Pet' }).getByText('Jean Coleman')).toBeVisible()
 
         //6. Click on the calendar icon and select the current date in date picker
-        const calendarInputField = page.getByRole('button', { name: 'Open calendar' })
         let currentDate = new Date()
-        const currentDay = currentDate.getDate().toString().padStart(2, '0')
-        const currentMonth = Number(currentDate.getMonth()+ 1).toString().padStart(2, '0')
-        await calendarInputField.click()
-        await page.getByRole('gridcell', { name: `/${currentMonth}/${currentDay}` }).click()
+        const currentDay = currentDate.getDate().toLocaleString('En-US', { minimumIntegerDigits: 2 })
+        const currentMonth = currentDate.toLocaleString('En-US', { month: '2-digit' })
+        const expectedYear = currentDate.getFullYear()
+        const currentDateFormatted = `${expectedYear}/${currentMonth}/${currentDay}`
+        await page.getByRole('button', { name: 'Open calendar' }).click()
+        await page.getByRole('button', { name: currentDateFormatted }).click()
 
         //7. Add an assertion that the selected date is displayed and it is in the format "YYYY/MM/DD"
-        const selectedDate = `${currentDate.getFullYear()}/${currentMonth}/${currentDay}`
-        await expect(page.locator('input[name="date"]')).toHaveValue(selectedDate)
+        await expect(page.locator('input[name="date"]')).toHaveValue(currentDateFormatted)
 
         //8. Type the description in the field, for example, "dermatologists visit" and click "Add Visit" button
         await page.locator('#description').fill('dermatologists visit')
         await page.getByRole('button', { name: 'Add Visit' }).click()
 
         //9. Add an assertion that the selected date of visit is displayed at the top of the list of visits for "Samantha" pet on the "Owner Information" page and is in the format "YYYY-MM-DD"
-        await expect(samanthaPetSection.locator('app-visit-list tr').nth(1).locator('td').first()).toHaveText(selectedDate.replace(/\//g, '-'))
+        await expect(samanthaPetSection.locator('app-visit-list tr').nth(1).locator('td').first()).toHaveText(currentDateFormatted.replace(/\//g, '-'))
 
         //10. Add one more visit for "Samantha" pet by clicking "Add Visit" button
         await samanthaPetSection.getByRole('button', { name: 'Add Visit' }).click()
 
         //11. Click on the calendar icon and select the date which is 45 days back from the current date
-        await calendarInputField.click()
-        let date = new Date()
-        date.setDate(date.getDate() - 45)
-        const expectedDay = date.getDate().toString().padStart(2, '0')
-        const expectedMonth = (date.getMonth() + 1).toString().padStart(2, '0')
-        const expectedYear = date.getFullYear()
-        const expectedDate = `${expectedYear}/${expectedMonth}/${expectedDay}`
+        await page.getByRole('button', { name: 'Open calendar' }).click()
+        currentDate.setDate(currentDate.getDate() - 45)
+        const expectedDay = currentDate.getDate().toLocaleString('En-US', { minimumIntegerDigits: 2 })
+        const expectedMonth = currentDate.toLocaleString('En-US', { month: '2-digit' })
+        const date45DaysBack = `${expectedYear}/${expectedMonth}/${expectedDay}`
 
-        let calendarMonthAndYear = await page.getByRole('button', { name: 'Choose month and year' }).textContent() ?? ''
+        let calendarMonthAndYear = await page.getByRole('button', { name: 'Choose month and year' }).textContent()!
         const expectedMonthAndYear = `${expectedMonth} ${expectedYear}`
 
-        while (!calendarMonthAndYear.includes(expectedMonthAndYear)) {
+        while (!calendarMonthAndYear!.includes(expectedMonthAndYear)) {
             await page.getByRole('button', { name: 'Previous month' }).click()
-            calendarMonthAndYear =  await page.getByRole('button', { name: 'Choose month and year' }).textContent() ?? ''
+            calendarMonthAndYear =  await page.getByRole('button', { name: 'Choose month and year' }).textContent()!
         }
-        await page.getByRole('gridcell', { name: expectedDate }).click()
+        await page.getByRole('gridcell', { name: date45DaysBack }).click()
 
         //12. Type the description in the field, for example, "massage therapy" and click "Add Visit" button
         await page.locator('#description').fill('massage therapy')
@@ -118,18 +116,17 @@ test.beforeEach( async({page}) => {
         //13. Add the assertion that the date added at step 11 is in chronological order in relation to the previous dates for "Samantha" pet on the "Owner Information" page. The date of visit above this date in the table should be greater.
         const dateAboveText = await samanthaPetSection.locator('app-visit-list tr').nth(1).locator('td').first().textContent()
         const newVisitDateText = await samanthaPetSection.locator('app-visit-list tr').nth(2).locator('td').first().textContent()
-        const dateAbove = new Date(dateAboveText ?? '')
-        const newVisitDate = new Date(newVisitDateText ?? '')
+        const dateAbove = new Date(dateAboveText!)
+        const newVisitDate = new Date(newVisitDateText!)
 
-        expect(dateAbove > newVisitDate).toBeTruthy() // or falsy:
-        // expect(dateAbove < newVisitDate).toBeFalsy()
+        expect(dateAbove > newVisitDate).toBeTruthy()
 
         //14. Select the "Delete Visit" button for both newly created visits
         await samanthaPetSection.locator('app-visit-list tr').nth(1).getByRole('button', { name: 'Delete Visit' }).click()
         await samanthaPetSection.locator('app-visit-list tr').nth(1).getByRole('button', { name: 'Delete Visit' }).click()
 
         //15. Add the assertion that deleted visits are no longer displayed in the table on "Owner Information" page
-        await expect(samanthaPetSection.locator('app-visit-list tr').nth(1).locator('td').first()).not.toHaveText(selectedDate.replace(/\//g, '-'))
-        await expect(samanthaPetSection.locator('app-visit-list tr').nth(1).locator('td').first()).not.toHaveText(expectedDate.replace(/\//g, '-'))        
+        await expect(samanthaPetSection.locator('app-visit-list tr').nth(1).locator('td').first()).not.toHaveText(currentDateFormatted.replace(/\//g, '-'))
+        await expect(samanthaPetSection.locator('app-visit-list tr').nth(1).locator('td').first()).not.toHaveText(date45DaysBack.replace(/\//g, '-'))        
     })
 })
